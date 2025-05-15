a02fa73ce989f98503be7372ce18c602dcbc36b4
rvgrisz <victorcarvalho.ru@hotmail.com>
2025-05-15 09:26:54 -0300

    Updated

--- beginner/C/1007.c
+++ beginner/C/1007.c
@@ -5,7 +5,7 @@
     The input file contains 4 integer values.
     
     Output
-    Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters,
+    Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters ,
     according to the following example, with a blank space before and after the equal signal.*/
 
 #include <stdio.h>
@@ -13,7 +13,7 @@
 
 int main (){
 
-    int A,B,C,D;
+    int A,B,C,D;   
 	
 	scanf("%d%d%d%d",&A,&B,&C,&D);
     printf("DIFERENCA = %d\n",(A*B - C*D));  /*e.g.: DIFERENCA = -26*/
